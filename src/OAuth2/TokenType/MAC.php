5b0756a795c25bcf6969b0c74b833edaa57d85be
Brent Shaffer <bshafs@gmail.com>
2013-01-17 22:53:29 -0700

    initial add of MAC token - parsing the data

--- src/OAuth2/TokenType/MAC.php
+++ src/OAuth2/TokenType/MAC.php
@@ -3,8 +3,10 @@
 /**
 * This is not yet supported!
 */
-class OAuth2_TokenType_MAC implements OAuth2_TokenTypeInterface
+class OAuth2_TokenType_MAC extends OAuth2_TokenType_Bearer
 {
+    private $response;
+
     public function getTokenType()
     {
         return 'mac';
@@ -12,7 +14,30 @@ class OAuth2_TokenType_MAC implements OAuth2_TokenTypeInterface
 
     public function getAccessTokenParameter(OAuth2_RequestInterface $request)
     {
-        throw new LogicException("Not supported");
+        $headers = $request->headers('AUTHORIZATION');
 
+        if (empty($headers) || 0 !== strpos($headers, 'MAC ')) {
+            $this->response = new OAuth2_Response_Error(400, 'invalid_request', 'Malformed auth header');
+            return null;
+        }
+
+        $macInfo = array();
+
+        $tokens = explode(',', trim(str_replace('MAC', '', $headers)));
+
+            // In the format Key="Value"
+        foreach ($tokens as $token) {
+            list($key, $value) = preg_split("/\=/", trim($token), 2);
+
+            //strip quotes off value
+            $macInfo[$key] = preg_replace('/^"|"$/', '', $value);
+        }
+
+        return $macInfo['id'];
+    }
+
+    public function getResponse()
+    {
+        return $this->response;
     }
 }
